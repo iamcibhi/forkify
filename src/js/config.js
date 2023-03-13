b4803ec6277a37b87cd4f7509542587c20a76917
iamcibhi <iamcibhi@gmail.com>
2023-03-13 20:56:22 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 13;
+export const KEY = 'fd10f936-f4b9-46e2-86f3-55c03821b47b';
+export const MODAL_CLOSE_SEC = 2.5;
